739b2d0803cf37b14226b5b420b422db98864827
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2026-05-18 22:13:58 -0300

    Add config.py with project configuration settings

---
+++ config.py
@@ -0,0 +1,31 @@
+
+
+PASTA_RAIZ = r"C:/Users/alo/Documentos/Project"
+
+NOME_MAPA = ''
+
+PASTA_COPIA = r"C:/Users/alo/Documentos/TEMP"
+
+SUFIXO_COPIA = '_copia'
+
+PASTA_XLSX = r"C:/Users/alo/Documentos/Project-XLSX"
+
+BUSCA_RECURSIVA = True
+
+PROJECT_VISIVEL = False
+
+COLUNAS = [
+    ('Grupo_de_recursos', 'ResourceGroup'),
+    ('OP', 'Text21'),
+    ('ÁREA_KPE', 'Text4'),
+    ('Nome', 'Name'),
+    ('Porcentagem_trabalho_concluído', 'PercentWorkComplete'),
+    ('Trabalho', 'Work'),
+    ('Duração', 'Duration'),
+    ('Início', 'Start'),
+    ('Término', 'Finish'),
+    ('Início_da_Linha_de_Base', 'BaselineStart'),
+    ('Término_da_linha_de_base', 'BaselineFinish'),
+    ('Resumo', 'Summary'),
+    ('Ativo', 'Active'),
+]
